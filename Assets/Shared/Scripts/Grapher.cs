758ab21caf29eff2e0feaf12c19b6bcd55ab978d
Can Olcer <shafyy@gmail.com>
2019-05-17 15:54:11 +0300

    finished first version of grapher

--- Assets/Shared/Scripts/Grapher.cs
+++ Assets/Shared/Scripts/Grapher.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
 
 namespace Kosmos
 {
   // Creates a graph
   public class Grapher : MonoBehaviour {
 
-    private int blockWidth;
-    private int blockHeight;
+    private Color[] colorsAxis;
+    private Color[] colorsGrid;
+    private int blockWidthLine;
+    private int blockHeightLine;
+    private int blockWidthGrid;
+    private int blockHeightGrid;
     private int border;
     private int lengthX;
     private int lengthY;
-    private Color[] colorsAxis;
+    private int nGridLines;
+    private float[] xAxisLabelValues;
+    private float[] yAxisLabelValues;
+    private List<float> predefinedAxisLabels;
     private Texture2D texture;
     private Renderer renderer;
+
+    [SerializeField] private GameObject AxisLabelPrefab;
+    [SerializeField] private Transform AxisLabelParentTransform;
+    [SerializeField] private string xAxisUnit;
+    [SerializeField] private string yAxisUnit;
+    [SerializeField] private string xAxisTitle;
+    [SerializeField] private string yAxisTitle;
+    [SerializeField] private TextMeshPro xAxisTMP;
+    [SerializeField] private TextMeshPro yAxisTMP;
     
 
     void Start() {
@@ -27,29 +44,47 @@ namespace Kosmos
 
       // define colors array to be used with SetPixels block overload
       // the color is a flattened 2D array that has the length blockWidth * blockHeight
-      blockWidth = 4;
-      blockHeight = 4;
-      colorsAxis = Enumerable.Repeat<Color>(Color.gray, blockWidth * blockHeight).ToArray<Color>();
+      blockWidthLine = 4;
+      blockHeightLine = 4;
+      blockWidthGrid = 2;
+      blockHeightGrid = 2;
+
+      // axis color
+      colorsAxis = Enumerable.Repeat<Color>(Color.gray, blockWidthLine * blockHeightLine).ToArray<Color>();
+
+      // define the number of gridlines per axis
+      nGridLines = 5;
+      // color light gray
+      colorsGrid = Enumerable.Repeat<Color>(new Color(0.9f, 0.9f, 0.9f), blockWidthGrid * blockHeightGrid).ToArray<Color>();
 
       // define border size and lengths of axes
-      border = 16;
+      border = 42;
+      // get lengths
       lengthX = texture.width - 2 * border;
       lengthY = texture.height - 2 * border;
+      // make sure it's divisble by number of gridlines
+      lengthX = lengthX - (lengthX % nGridLines);
+      lengthY = lengthY - (lengthY % nGridLines);
 
-      // draw x and y axes
-      drawAxis("x",colorsAxis);
-      drawAxis("y",colorsAxis);
+      // these are predefined grid stepss
+      predefinedAxisLabels = new List<float> {0.1f, 0.2f, 0.5f, 1f, 2f, 10f, 20f, 50f, 100f, 200f, 500f, 1000f};
 
-      int[] exampleX = new int[5] {10, 15, 20, 25, 30};
-      int[] exampleY = new int[5] {30, 30, 40, 60, 65};
-      Color[] exampleColors = Enumerable.Repeat<Color>(Color.magenta, blockWidth * blockHeight).ToArray<Color>();
+      float[] exampleX = new float[5] {10f, 15f, 20f, 25f, 30f};
+      float[] exampleY = new float[5] {0.2f, 0.3f, 0.33f, 0.71f, 0.82f};
+      Color[] exampleColors = Enumerable.Repeat<Color>(Color.magenta, blockWidthLine * blockHeightLine).ToArray<Color>();
       GraphLine(exampleX, exampleY, exampleColors);
 
     }
 
     // draws a block of pixels (don't forget to call texture.Apply())
-    private void drawPixelBlock(int x, int y, Color[] colors) {
+    // make sure that blockWidth * blockHeight = colors.Length
+    private void drawPixelBlock(int x, int y, int blockWidth, int blockHeight, Color[] colors) {
       texture.SetPixels(x, y, blockWidth, blockHeight, colors, 0);
+    }
+
+    private void setAxesTitles() {
+      xAxisTMP.text = xAxisTitle + " [" + xAxisUnit + "]";
+      yAxisTMP.text = yAxisTitle + " [" + yAxisUnit + "]";
     }
 
     // draw x or y axis for graph
@@ -57,25 +92,32 @@ namespace Kosmos
       if (axis == "x") {
         // draw line
         // draw pixel blocks, make sure to increase i by blockWidth
-        for (int i = 0; i < lengthX; i += blockWidth) {
-          drawPixelBlock(i + border, border, colors);
+        for (int i = 0; i < lengthX; i += blockWidthLine) {
+          drawPixelBlock(i + border, border, blockWidthLine, blockHeightLine, colors);
         }
+        // add one more pixel block but starting blockWidthGrid earlier so that the grid isn't
+        // wider than the axis
+        drawPixelBlock(lengthX + border - blockWidthGrid, border, blockWidthLine, blockHeightLine, colors);
 
         // draw arrow head
         // get last point using modus
-        int lastPoint = border + lengthX - (lengthX % blockWidth);
-        drawArrowHead("x", lastPoint, border);
+        //int lastPoint = border + lengthX - (lengthX % blockWidthLine);
+        //drawArrowHead("x", lastPoint, border);
       } else {
           // draw line
           // draw pixel blocks, make sure to increase i by blockHeight
-          for (int i = 0; i < lengthY; i += blockHeight) {
-            drawPixelBlock(border, i + border, colors);
+          for (int i = 0; i < lengthY; i += blockHeightLine) {
+            drawPixelBlock(border, i + border, blockWidthLine, blockHeightLine, colors);
           }
+
+          // add one more pixel block but starting blockHeightGrid earlier so that the grid isn't
+          // higher than the axis
+          drawPixelBlock(border, lengthY + border - blockHeightGrid, blockWidthLine, blockHeightLine, colors);
 
           // draw arrow head
           // get last point using modus
-          int lastPoint = border + lengthY - (lengthY % blockHeight);
-          drawArrowHead("y", border, lastPoint);
+          //int lastPoint = border + lengthY - (lengthY % blockHeightLine);
+          //drawArrowHead("y", border, lastPoint);
       }
 
       // upload texture to graphics card
@@ -86,36 +128,195 @@ namespace Kosmos
     private void drawArrowHead(string axis, int startingPointX, int startingPointY) {
       if (axis == "x") {
         // upper part
-        drawPixelBlock(startingPointX - (2 * blockWidth), startingPointY + blockHeight, colorsAxis);
-        drawPixelBlock(startingPointX - (3 * blockWidth), startingPointY + (2 * blockHeight), colorsAxis);
+        drawPixelBlock(startingPointX - (2 * blockWidthLine), startingPointY + blockHeightLine, blockWidthLine, blockHeightLine, colorsAxis);
+        drawPixelBlock(startingPointX - (3 * blockWidthLine), startingPointY + (2 * blockHeightLine), blockWidthLine, blockHeightLine, colorsAxis);
 
         // lower part
-        drawPixelBlock(startingPointX - (2 * blockWidth), startingPointY - blockHeight, colorsAxis);
-        drawPixelBlock(startingPointX - (3 * blockWidth), startingPointY - (2 * blockHeight), colorsAxis);
+        drawPixelBlock(startingPointX - (2 * blockWidthLine), startingPointY - blockHeightLine, blockWidthLine, blockHeightLine, colorsAxis);
+        drawPixelBlock(startingPointX - (3 * blockWidthLine), startingPointY - (2 * blockHeightLine), blockWidthLine, blockHeightLine, colorsAxis);
       } else {
         // right part
-        drawPixelBlock(startingPointX + blockWidth, startingPointY - (2 * blockHeight), colorsAxis);
-        drawPixelBlock(startingPointX + (2 * blockWidth), startingPointY - (3 * blockHeight), colorsAxis);
+        drawPixelBlock(startingPointX + blockWidthLine, startingPointY - (2 * blockHeightLine), blockWidthLine, blockHeightLine, colorsAxis);
+        drawPixelBlock(startingPointX + (2 * blockWidthLine), startingPointY - (3 * blockHeightLine), blockWidthLine, blockHeightLine, colorsAxis);
 
         // left part
-        drawPixelBlock(startingPointX - blockWidth, startingPointY - (2 * blockHeight), colorsAxis);
-        drawPixelBlock(startingPointX - (2 * blockWidth), startingPointY - (3 * blockHeight), colorsAxis);
+        drawPixelBlock(startingPointX - blockWidthLine, startingPointY - (2 * blockHeightLine), blockWidthLine, blockHeightLine, colorsAxis);
+        drawPixelBlock(startingPointX - (2 * blockWidthLine), startingPointY - (3 * blockHeightLine), blockWidthLine, blockHeightLine, colorsAxis);
 
         }
     }
 
+    // draws grid with nGridLines lines per axis
+    private void drawGrid() {
+      // define vars
+      float relativeXPos;
+      float relativeYPos;
+      float quadXPos;
+      float quadYPos;
+
+      // x axis grid (horizontal)
+
+      // get 1/nGridLines of lengthY, use modulo to make sure we don't get floats
+      // this is not really necessary as the length should be already perfectly
+      // divisble from our Start() function
+      int xGridHeight = (lengthY - (lengthY % nGridLines)) / nGridLines;
+      // draw grid lines
+      for (int i = 0; i <= nGridLines; i++) {
+        int yPos = border + i * xGridHeight;
+        // length should be as long as x axis, make sure to increment by blockWidth
+        // don't draw gridline for the first one
+        if (i != 0) {
+          for (int y = blockWidthLine; y <= lengthX; y += blockWidthGrid) {
+            drawPixelBlock(border + y, yPos, blockWidthGrid, blockHeightGrid, colorsGrid);
+          }
+        }
+
+        // set label
+        // translate texture pixel to world space (Vector3)
+        relativeYPos = (float)yPos / (float)texture.height;
+        quadYPos = transform.localScale.y * relativeYPos;
+
+        relativeXPos = (float)(border - 2 * blockWidthLine) / (float)texture.width;
+        quadXPos = transform.localScale.x * relativeXPos;
+
+        // translate to Vector3
+        Vector3 labelPos = new Vector3(quadXPos, quadYPos, -0.0001f);
+
+        // set actual label
+        setAxesLabel(i, yAxisLabelValues, labelPos);
+      }
+
+      // y axis grid (vertical)
+      // get 1/nGridLines of lengthY, use modulo to make sure we don't get floats
+      // this is not really necessary as the length should be already perfectly
+      // divisble from our Start() function
+      int yGridHeight = (lengthX - (lengthX % nGridLines)) / nGridLines;
+      // draw grid lines
+      for (int i = 0; i <= nGridLines; i++) {
+        int xPos = border + i * yGridHeight;
+        // length should be as long as y axis, make sure to increment by blockWidth
+        // don't draw gridline for the first one
+        if (i != 0) {
+          for (int y = blockHeightLine; y <= lengthY; y += blockHeightGrid) {
+            drawPixelBlock(xPos, border + y, blockWidthGrid, blockHeightGrid, colorsGrid);
+          }
+        }
+
+        // set label
+        // translate texture pixel to world space (Vector3)
+        relativeXPos = (float)xPos / (float)texture.width;
+        quadXPos = transform.localScale.x * relativeXPos;
+
+        relativeYPos = (float)(border - 2 * blockHeightLine) / (float)texture.height;
+        quadYPos = transform.localScale.y * relativeYPos;
+
+        // translate to Vector3
+        Vector3 labelPos = new Vector3(quadXPos, quadYPos, -0.0001f);
+
+        // set actual label
+        setAxesLabel(i , xAxisLabelValues, labelPos);
+      }
+
+      texture.Apply();
+    }
+
+    // creates a label for axes
+    private void setAxesLabel(int number, float[] labelValues, Vector3 pos) {
+      // instantiate label at the specific pos and parent
+      GameObject newAxisLabel = (GameObject)Instantiate(AxisLabelPrefab, AxisLabelParentTransform);
+      newAxisLabel.transform.localPosition = pos;
+
+      // assign correct value to TextMeshPro
+      TextMeshPro currentTMP = newAxisLabel.GetComponentInChildren<TextMeshPro>();
+      // display decimal if necessary
+      if (Mathf.Abs(labelValues[number]) < 1.0f && Mathf.Abs(labelValues[number]) > 0.0f) {
+        currentTMP.SetText("{0:1}", labelValues[number]);
+      } else {
+        currentTMP.SetText("{0}", labelValues[number]);
+      }
+      
+    }
+
+    // creates arrays with axis label values based on max values as arguments
+    private float[] createAxisLabelValues(float maxValue, float minValue) {
+      // available grid steps:
+      // 0.1, 0.2, 0.5,
+      // 1, 2, 10, 20, 50,
+      // 100, 200, 500,
+      // 1000
+      
+      // divide max value by nGridLInes
+      float gridDivider = maxValue / nGridLines;
+      // always take the next biggest scale of maxValue / nGridLines
+
+      float gridStep = predefinedAxisLabels.OrderBy(l => l).FirstOrDefault(l => l > gridDivider);
+      
+      predefinedAxisLabels.OrderBy(l => l);
+
+      // if no higher value found
+      if (gridStep == 0f) {
+        Debug.Log("GraphLine error: Max Value too high for predefinedAxisLabels");
+        // default to 1000
+        gridStep = 1000f;
+      }
+
+      return new float[] {0, gridStep, gridStep * 2, gridStep * 3, gridStep * 4, gridStep * 5};
+
+
+    }
+
     // creates a line graph
-    public void GraphLine(int[] xValues, int[] yValues, Color[] colors) {
+    public void GraphLine(float[] xValues, float[] yValues, Color[] colors) {
       // check if lengths of x values and y values match, if not return
       if (xValues.Length != yValues.Length) {
         Debug.Log("GraphLine error: number of xValues and yValues need to be the same.");
         return;
       }
 
+      // get maximum and minimum values of x and y axes (used to scale the graph correctly)
+      float xMax = xValues.Max();
+      float yMax = yValues.Max();
+      float xMin = xValues.Min();
+      float yMin = yValues.Min();
+
+      // create arrays with axis label values
+      xAxisLabelValues = createAxisLabelValues(xMax, xMin);
+      yAxisLabelValues = createAxisLabelValues(yMax, yMin);
+
+      // // get ratio to length of axes
+      // float xFactor = (lengthX - blockWidthLine) / xMax;
+      // float yFactor = (lengthY - blockHeightLine) / yMax;
+
+      // // scale up / down values
+      // int[] scaledXValues = xValues.Select(x => (int)(x * xFactor)).ToArray();
+      // int[] scaledYValues = yValues.Select(x => (int)(x * yFactor)).ToArray();
+
+      float xLabelMax = xAxisLabelValues[nGridLines];
+      float yLabelMax = yAxisLabelValues[nGridLines];
+
+      // get ratio to length of axes
+      float xFactor = (lengthX - blockWidthLine) / xLabelMax;
+      float yFactor = (lengthY - blockHeightLine) / yLabelMax;
+
+      // scale up / down values
+      int[] scaledXValues = xValues.Select(x => (int)(x * xFactor)).ToArray();
+      int[] scaledYValues = yValues.Select(x => (int)(x * yFactor)).ToArray();
+      
+
+      // draw x and y axes
+      drawAxis("x", colorsAxis);
+      drawAxis("y", colorsAxis);
+
+      // set their description
+      setAxesTitles();
+
+      // draw grid
+      drawGrid();
+
       // loop through xValues and call drawPixelBlock for each xValue-yValue pair
       // add border so that it's drawn within the axes
-      for (int i = 0; i < xValues.Length; i++) {
-        drawPixelBlock(border + xValues[i], border + yValues[i], colors);
+      for (int i = 0; i < scaledXValues.Length; i++) {
+        drawPixelBlock(border + scaledXValues[i], border + scaledYValues[i], blockWidthLine, blockHeightLine, colors);
       }
 
       // upload to graphics card
